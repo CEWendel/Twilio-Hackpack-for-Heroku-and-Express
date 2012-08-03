efb372f3bd93d104410c5f6c770003da3754beaf
Chris Wendel <chriwend@umich.edu>
2012-08-03 19:21:12 -0400

    jeaaahhh

--- app.js
+++ app.js
@@ -17,16 +17,29 @@ var TwilioClient = require('node-twilio').Client,
 app.get("/", function(req, res) {
   var out = "hello", phone = client.getPhoneNumber('+14157234224');
   phone.setup(function() {
-    phone.sendSms("+17033891424", out, {}, function(text) {
-      console.log("Shit");
-      res.send("done");
-    });
+    //phone.sendSms("+17033891424", out, {}, function(text) {
+    //  console.log("Shit");
+    //  res.send("done");
+    //});
+  	phone.makeCall('+17033891424', null, function(call){
+  		call.on('answered', function(request, response){
+  			console.log("Call answered");
+  			response.send("<Response><Say>Thanks!</Say></Response>");
+  		});
+  		call.on('ended', function(request, response){
+  			console.log("call ended");
+  	 	});
+  	});
   });
 });
 
 app.post("/incoming/sms", function(req, res) {
   console.log("incoming sms!");
   res.send("<Response><Sms>Thanks!</Sms></Response>");
+});
+
+app.get("/incoming/sms", function(req, res){
+	res.send("<Response><Sms>Thanks!</Sms></Response>");
 });
 
 app.listen(process.env.PORT || config.port);
