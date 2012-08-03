2a10d99d16d13ac0547d6c7d2276b4b39bd7d04f
Chris Wendel <chriwend@umich.edu>
2012-08-03 05:13:29 -0400

    yeaaa

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ config = {};
 config.TWILIO_ACCOUNT_SID = 'ACefb267919ab7c793e889ce40b8db2506';
 config.TWILIO_AUTH_TOKEN = '6cb0a97591eaf94ca237572fe4472458';
 config.HOST = 'twiliohackpack.herokuapp.com';
-var TwilioClient = require('./node-twilio').Client,
-  Twiml = require('./node-twilio').Twiml,
+var TwilioClient = require( __dirname + '/node-twilio').Client,
+  Twiml = require(__dirname + '/node-twilio').Twiml,
   client = new TwilioClient(config.TWILIO_ACCOUNT_SID, config.TWILIO_AUTH_TOKEN , config.HOST, {
     "express" : app,
     "port" : process.env.PORT || config.port
   });
-  
+
 app.get("/", function(req, res) {
   var out = "hello", phone = client.getPhoneNumber('+14157234224');
   phone.setup(function() {
